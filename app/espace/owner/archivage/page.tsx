00d3237c49170b35d5fa556060542612f9e3e635
wafwana <recrutement.prive@hotmail.com>
2026-09-23 15:09:46 +0200

    Fix client directive order for Vercel build

--- app/espace/owner/archivage/page.tsx
+++ app/espace/owner/archivage/page.tsx
@@ -1,5 +1,7 @@
-import BackButton from "@/components/navigation/BackButton";
 "use client";
+
+import BackButton from "@/components/navigation/BackButton";
+
 
 import { useEffect, useState } from "react";
 
